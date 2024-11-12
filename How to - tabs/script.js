20cca96858077fd7afe1ee6426e0f7c36a5463fd
saurabh <saurabh@gmail.com>
2024-11-12 10:07:56 +0530

    added code

---
+++ How to - tabs/script.js
@@ -0,0 +1,15 @@
+const divs = document.querySelectorAll(".tabcontent");
+         const parent = document.querySelector(".parent");
+        
+        const openCity = (city) => {
+          console.log(city);
+           for(let i = 0; i < divs.length; i++) {
+            parent.classList.add("border");
+            if(divs[i].id === city){
+              divs[i].classList.remove("hidden");
+            }
+            else{
+              divs[i].classList.add("hidden");
+            }
+          }
+        }
