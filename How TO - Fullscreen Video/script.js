cd669e2078bef67d4922530b47a12353ebb31e47
saurabh <saurabh@gmail.com>
2024-11-26 19:14:35 +0530

    done

--- How TO - Fullscreen Video/script.js
+++ How TO - Fullscreen Video/script.js
@@ -2,7 +2,6 @@ const btn = document.querySelector('#btn');
 const video = document.querySelector('video');
 
 btn.addEventListener('click', () => {
-    console.log(video.paused);
     btn.innerText = btn.innerText === 'Play' ? 'Pause' : 'Play';
     if (video.paused) {
         video.play();
